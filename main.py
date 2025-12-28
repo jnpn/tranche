bc65fe695da4ce9a4fdd98c60b7faa37ecab69e2
Johan PONIN <johan.ponin.pro@gmail.com>
2025-12-28 02:24:55 +0100

    feat: initial

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+branch("dev") > transition() > branch("staging") > transition() > branch("main")
+
+# or
+
+branch("dev") > branch("staging") > branch("main")
+
+dev = branch("dev")
+staging = branch("staging")
+main = branch("main")
+
+dev > staging > main
+
+staging.when_merged(lambda ctx: os.exec("bumpversion --tag"))
+main.when_merged(lambda ctx: os.exec("bumpversion --tag"))
+"""
+
+# prom repo promotion state
+# prom-start br [a>b>c] nil
+# prom a [a>b>c] nil
+# prom-merge a b
+# prom-eff (eff a b)
+# prom b [b>c] [b':b]
+# prom-merge b c
+# prom-eff (eff b c)
+# prom c [c] [c':c, b':b]
+# prom-end c [c':c, b':b]
+
+def main():
+    print("Hello from glisse!")
+
+
+if __name__ == "__main__":
+    main()
